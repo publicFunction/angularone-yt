91de6584a3f27a0c1fe6d7ff727b3c93062ea983
Chris Moss <Chris Moss>
2016-07-20 22:57:58 +0100

    Nowt important
    Signed-off-by: Chris Moss <Chris Moss>

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,17 @@ gulp.task('hello', function() {
     console.log("Hello World!")
 });
 
+gulp.task('watch', function() {
+    gulp.watch('sass/**/*.scss', ['sass']);
+});
+
 gulp.task('sass', function() {
-    return gulp.src('sass/*')
+    return gulp.src('sass/**/*.scss')
         .pipe(sass())
         .pipe(gulp.dest('styles/'))
+});
+
+gulp.task('js', function() {
+    return gulp.src('js/**/*.js')
+        .pipe(gulp.dest('javascripts/'))
 });
